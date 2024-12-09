325b0d3d1569cfc8fc56947958562c43425e9d15
guillaume prof <guillaume.saintetienne@iter.org>
2024-12-09 16:47:30 +0100

    US #2 complétée

--- src/test/kotlin/org/example/montaine/guillaume/tests/GuillaumeTest.kt
+++ src/test/kotlin/org/example/montaine/guillaume/tests/GuillaumeTest.kt
@@ -3,17 +3,17 @@ package org.example.montaine.guillaume.tests
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldNotBe
-import montaine.guillaume.TicketDeCaisse
+import montaine.guillaume.Commande
 
 class GuillaumeTest : BehaviorSpec({
 
     context("je veux vérifier que chaque ticket a son identifiant unique") {
 
         given("un ticket") {
-            val ticket = TicketDeCaisse()
+            val ticket = Commande()
 
             And("un 2e ticket") {
-                val ticket2 = TicketDeCaisse()
+                val ticket2 = Commande()
 
                 When("je lis les 2 identifiants") {
                     val id1 = ticket.Id
--- src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
+++ src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
@@ -2,9 +2,67 @@
 package org.example.montaine.guillaume.tests
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import montaine.guillaume.Commande
 
 class MontaineGuillaumeTest: BehaviorSpec({
 
-    context("je veux vérifier les reductions par état et selon les montants") {
+    context(
+            "Calcul du prix d'une commande simple en France") {
+        val commande = Commande()
+
+        //Exemples - Règle: Calcul standard avec nombres entiers
+        given("je vends 5 mangas à 10€ chacun "){
+
+            commande.VendsLotManga( 5, 10.0)
+
+            When("je calcule le prix total"){
+                val prixTotal = commande.PrixTotal()
+
+                Then("le prix total est de 60"){
+                    prixTotal shouldBe 60.0
+                }
+            }
+        }
+
+        given("je vends 3 mangas à 20€ chacun "){
+
+            commande.VendsLotManga( 3, 20.0)
+
+            When("je calcule le prix total"){
+                val prixTotal = commande.PrixTotal()
+
+                Then("le prix total est de 72"){
+                    prixTotal shouldBe 72
+                }
+            }
+        }
+
+        //
+        given("je vends 3 mangas à 9.99€ chacun "){
+
+            commande.VendsLotManga( 3, 9.99)
+
+            When("je calcule le prix total"){
+                val prixTotal = commande.PrixTotal()
+
+                Then("le prix total est de 35.64"){
+                    prixTotal shouldBe 35.64
+                }
+            }
+        }
+
+        given("je vends 7 mangas à 19.99€ chacun "){
+
+            commande.VendsLotManga( 7, 19.99)
+
+            When("je calcule le prix total"){
+                val prixTotal = commande.PrixTotal()
+
+                Then("le prix total est de  167.92"){
+                    prixTotal shouldBe 167.92
+                }
+            }
+        }
 
     }})
